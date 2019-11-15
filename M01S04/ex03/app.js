f2405990e26fa91247b255ef47c8f40ccf6b4796
ValentinaTase <55147151+ValentinaTase@users.noreply.github.com>
2019-11-15 19:46:37 +0200

    js04

---
+++ M01S04/ex03/app.js
@@ -0,0 +1,18 @@
+var errorCode = 200;
+
+switch (errorCode) {
+  case 100:
+    console.log('100');
+  break;
+  case 200:
+    console.log('200');
+  break;
+  case 300:
+    console.log('300');
+  break;
+  case 400:
+    console.log('400');
+  break;
+  default:
+    console.log('something else');
+}
---
+++ M01S04/ex04/app.js
@@ -0,0 +1,10 @@
+var i;
+
+
+
+for ( i = 0; i <= 100; i ++) {
+  if (i % 2 !== 0) {
+    continue;
+  }
+  console.log(i);
+}
