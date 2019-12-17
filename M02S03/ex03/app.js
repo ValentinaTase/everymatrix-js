1269f695a52d5a4308bd84c4ec525b1407e35111
ValentinaTase <55147151+ValentinaTase@users.noreply.github.com>
2019-12-17 19:57:40 +0200

    17 dec

--- M02S03/ex03/app.js
+++ M02S03/ex03/app.js
@@ -1,1 +1,14 @@
+let stage = document.querySelector('.stage');
+let message = document.querySelector('.message');
+let countElement = document.querySelector('.count');
+let count = 0;
 
+stage.addEventListener('mouseover', () => {
+message.innerText = 'Mouseul este pe scena';
+count = count + 1; 
+});
+
+stage.addEventListener('mouseout', () => {
+  message.innerText = 'Mouseul nu este pe scena';
+  countElement.innerText = `Mouseul a fost pe scena de ${count} ori.`;
+});
