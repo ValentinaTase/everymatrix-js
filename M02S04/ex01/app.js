5df144973562c56a42de86fda1b6e1cb5fbcc638
ValentinaTase <55147151+ValentinaTase@users.noreply.github.com>
2019-12-19 20:32:51 +0200

    19 Decembrie

--- M02S04/ex01/app.js
+++ M02S04/ex01/app.js
@@ -1,5 +1,6 @@
 let boxElement = document.querySelector('.box');
 let applyButton = document.querySelector('#applyButton');
+let darkenButton = document.getElementById('darkenButton');
 let cssClassName = 'animate-class';
 
 window.setTimeout(() => {
@@ -10,6 +11,17 @@ window.setTimeout(() => {
   }, 2000);
 }, 2000);
 
-applyButton.addEventListener('click', () => {
-  boxElement.classList.toggle(cssClassName);
+applyButton.addEventListener('click', (evt) => {
+  // boxElement.classList.toggle(cssClassName);
+  if (boxElement.classList.contains(cssClassName)) {
+    boxElement.classList.remove(cssClassName);
+    evt.target.innerText = 'Aplica';
+  } else {
+    boxElement.classList.add(cssClassName);
+    evt.target.innerText = 'Elimina';
+  }
+});
+
+darkenButton.addEventListener('click', () => {
+  boxElement.style.backgroundColor = '#000';
 });
